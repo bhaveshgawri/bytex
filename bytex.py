910409db2f66fbe0aa7c521243d1ffad576f793c
vector-44 <4bhaveshgawri@gmail.com>
2016-12-18 12:09:23 +0530

    edit menu functions added

--- bytex.py
+++ bytex.py
@@ -8,6 +8,7 @@ class Editor(QtGui.QMainWindow):
 		self.setWindowTitle("bytex")
 		#self.setWindowIcon(QtGui.QIcon())
 		
+#--------------------File Menu Actions-------------------#
 
 		newTabAction = QtGui.QAction("New File",self)
 		newTabAction.setShortcut("Ctrl+t")
@@ -44,44 +45,62 @@ class Editor(QtGui.QMainWindow):
 		quitAction.setStatusTip("Close all documents and EXIT.")
 		quitAction.triggered.connect(self.close_editor)
 
-		cutAction = QtGui.QAction("Cut",self)
-		cutAction.setShortcut("Ctrl+x")
-		cutAction.setStatusTip("Cut the selected text.")
-		cutAction.triggered.connect(self.close_editor)
-		
-		copyAction = QtGui.QAction("Copy",self)
-		copyAction.setShortcut("Ctrl+c")
-		copyAction.setStatusTip("Copy the selected text.")
-		copyAction.triggered.connect(self.close_editor)
-		
-		pasteAction = QtGui.QAction("Paste",self)
-		pasteAction.setShortcut("Ctrl+v")
-		pasteAction.setStatusTip("Paste the text on clipboard at position of cursor.")
-		pasteAction.triggered.connect(self.close_editor)
-		
+#------------------//File Menu Actions//-----------------#
+
+#--------------------Edit Menu Actions-------------------#
+
 		undoAction = QtGui.QAction("Undo",self)
 		undoAction.setShortcut("Ctrl+z")
 		undoAction.setStatusTip("Undo the last change.")
-		undoAction.triggered.connect(self.close_editor)
+		undoAction.triggered.connect(self.undo_)
 		
 		redoAction = QtGui.QAction("Redo",self)
 		redoAction.setShortcut("Ctrl+Shift+z")
 		redoAction.setStatusTip("Redo the last change.")
-		redoAction.triggered.connect(self.close_editor)
+		redoAction.triggered.connect(self.redo_)
 		
-		deleteAction = QtGui.QAction("Delete",self)
-		deleteAction.setShortcut("del")
-		deleteAction.setStatusTip("Delete the selected text.")
-		deleteAction.triggered.connect(self.close_editor)
+		cutAction = QtGui.QAction("Cut",self)
+		cutAction.setShortcut("Ctrl+x")
+		cutAction.setStatusTip("Cut the selected text.")
+		cutAction.triggered.connect(self.cut_)
+		
+		copyAction = QtGui.QAction("Copy",self)
+		copyAction.setShortcut("Ctrl+c")
+		copyAction.setStatusTip("Copy the selected text.")
+		copyAction.triggered.connect(self.copy_)
+		
+		pasteAction = QtGui.QAction("Paste",self)
+		pasteAction.setShortcut("Ctrl+v")
+		pasteAction.setStatusTip("Paste the text on clipboard at position of cursor.")
+		pasteAction.triggered.connect(self.paste_)
 		
 		readOnlyAction = QtGui.QAction("Read Only",self, checkable = True)
 		readOnlyAction.setStatusTip("Make text read-only.")
-		readOnlyAction.triggered.connect(lambda: self.read_only(readOnlyAction))
+		readOnlyAction.triggered.connect(lambda: self.readOnly_(readOnlyAction))
 		
+		selectAllAction = QtGui.QAction("Select All",self)
+		selectAllAction.setShortcut("Ctrl+a")
+		selectAllAction.setStatusTip("Select all the text.")
+		selectAllAction.triggered.connect(self.selectAll_)
+
+		clearAction = QtGui.QAction("Clear Screen",self)
+		clearAction.setShortcut("Ctrl+l")
+		clearAction.setStatusTip("Clear all the text from screen.")
+		clearAction.triggered.connect(self.clear_)
+		
+		insertAction = QtGui.QAction("Inseret Mode",self, checkable = True)
+		insertAction.setStatusTip("Toggle Insert Mode.")
+		insertAction.triggered.connect(lambda: self.insert_(insertAction))
+		
+#-------------------//Edit Menu Actions//-----------------#		
+
+#---------------------Menu Bar Creation-------------------#
+
 		bar = self.menuBar()
 		file = bar.addMenu("File")
 		edit = bar.addMenu("Edit")
 		
+
 		file.addAction(newTabAction)
 		file.addSeparator()
 		file.addAction(openAction)
@@ -103,28 +122,71 @@ class Editor(QtGui.QMainWindow):
 		edit.addSeparator()
 		edit.addAction(readOnlyAction)
 		edit.addSeparator()
-		edit.addAction(deleteAction)
+		edit.addAction(selectAllAction)
+		edit.addAction(clearAction)
+		edit.addSeparator()
+		edit.addAction(insertAction)
 
+#-------------------//Menu Bar Creation//-----------------#
+		
+		self.statusBar()
 		self.editor()
-
 		self.view()
+
+#-------------------Function Declarations-----------------#
 	
 	def view(self):
 		self.show()
-	
 	def editor(self):
 		self.textEdit = QtGui.QTextEdit()
 		self.setCentralWidget(self.textEdit)
 	
+#--------------------File Menu Functions------------------#	
+	
 	def close_editor(self):
 		sys.exit()
+
+
+#------------------//File Menu Functions//----------------#
+
+#--------------------Edit Menu Functions------------------#
+
+	def undo_(self):
+		self.textEdit.undo()
 	
-	def read_only(self, readOnlyAction):
+	def redo_(self):
+		self.textEdit.redo()
+	
+	def cut_(self):
+		self.textEdit.cut()
+	
+	def copy_(self):
+		self.textEdit.copy()
+	
+	def paste_(self):
+		self.textEdit.paste()
+	
+	def readOnly_(self, readOnlyAction):
 		if readOnlyAction.isChecked() is True:
 			self.textEdit.setReadOnly(True)
 		else:
 			self.textEdit.setReadOnly(False)
-		
+	
+	def selectAll_(self):
+		self.textEdit.selectAll()
+	
+	def clear_(self):
+		self.textEdit.clear()
+	
+	def insert_(self, insertAction):
+		if insertAction.isChecked() is True:
+			self.textEdit.setOverwriteMode(True)
+		else:
+			self.textEdit.setOverwriteMode(False)
+
+#------------------//Edit Menu Functions//----------------#
+	
+	
 def run():
 	app = QtGui.QApplication(sys.argv)
 	new_editor = Editor()

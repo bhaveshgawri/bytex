25b8939693096de59af5a89b3f0144ac6c91b0af
vector-44 <4bhaveshgawri@gmail.com>
2016-12-21 18:54:51 +0530

    moved to QsciScintilla from QTextEdit and func. made compatible with it

--- bytex.py
+++ bytex.py
@@ -1,11 +1,11 @@
-import sys,  os
-from PyQt4 import QtGui, QtCore
+import sys,  os, webbrowser, subprocess
+from PyQt4 import QtGui, QtCore, Qsci
 from markdown_editor import web_edit
-import webbrowser, subprocess
 
 class Editor(QtGui.QMainWindow):
 	def __init__(self):
-		super(Editor,self).__init__()
+		super(Editor, self).__init__()
+		#	print (help(Editor))
 		self.setGeometry(0,0, 600, 800)
 		self.setMinimumSize(600,350)
 		self.setWindowTitle("bytex")		
@@ -16,18 +16,17 @@ class Editor(QtGui.QMainWindow):
 		self.tabWidget.setMovable(True)
 		#self.tabWidget.setTabShape(QtGui.QTabWidget.Triangular)
 
-			
 #--------------------File Menu Actions-------------------#
 
 		newTabAction = QtGui.QAction("New File",self)
-		newTabAction.setShortcut("Ctrl+t")
+		newTabAction.setShortcut("Ctrl+e")
 		newTabAction.setStatusTip("Create a new Document")
 		newTabAction.triggered.connect(self.newFile_)
 
 		newWinAction = QtGui.QAction("New Window",self)
 		newWinAction.setShortcut("Ctrl+n")
 		newWinAction.setStatusTip("Open a new document in a new window")
-		newWinAction.triggered.connect(self.__init__)
+		newWinAction.triggered.connect(self.newWindow_)
 		
 		openAction = QtGui.QAction("Open",self)
 		openAction.setShortcut("Ctrl+o")
@@ -87,7 +86,7 @@ class Editor(QtGui.QMainWindow):
 		
 		readOnlyAction = QtGui.QAction("Read Only",self, checkable = True)
 		readOnlyAction.setStatusTip("Make text read-only.")
-		readOnlyAction.triggered.connect(lambda: self.readOnly_(readOnlyAction_))
+		readOnlyAction.triggered.connect(lambda: self.readOnly_(readOnlyAction))
 		
 		selectAllAction = QtGui.QAction("Select All",self)
 		selectAllAction.setShortcut("Ctrl+a")
@@ -115,7 +114,9 @@ class Editor(QtGui.QMainWindow):
 		action2 = action_group.addAction(QtGui.QAction("Tab Width:2", self, checkable=True))
 		action4 = action_group.addAction(QtGui.QAction("Tab Width:4", self, checkable=True))
 		action8 = action_group.addAction(QtGui.QAction("Tab Width:8", self, checkable=True))
-		action_group.triggered.connect(lambda: self.setTabWidth_(action_group, action2, action4, action8))
+		action12 = action_group.addAction(QtGui.QAction("Tab Width:12", self, checkable=True))
+		action16 = action_group.addAction(QtGui.QAction("Tab Width:16", self, checkable=True))
+		action_group.triggered.connect(lambda: self.setTabWidth_(action_group, action2, action4, action8, action12, action16))
 
 #------------------//Format Menu Actions//----------------#
 
@@ -131,7 +132,7 @@ class Editor(QtGui.QMainWindow):
 		newMarkDownFileAction.setStatusTip("Try the markdown editor now.")
 		newMarkDownFileAction.triggered.connect(self.openMarkEditor_)
 
-		openMarkDownFileAction = QtGui.QAction("Open",self)
+		openMarkDownFileAction = QtGui.QAction("Open File",self)
 		openMarkDownFileAction.setShortcut("ctrl+shift+o")
 		openMarkDownFileAction.setStatusTip("Open a file in markdown editor.")
 		openMarkDownFileAction.triggered.connect(self.openMarkEditorFile_)
@@ -206,6 +207,8 @@ class Editor(QtGui.QMainWindow):
 		tabWidth.addAction(action2)
 		tabWidth.addAction(action4)
 		tabWidth.addAction(action8)
+		tabWidth.addAction(action12)
+		tabWidth.addAction(action16)
 
 		tools.addAction(terminalAction)
 		tools.addSeparator()
@@ -244,25 +247,27 @@ class Editor(QtGui.QMainWindow):
 
 	def selectedText_(self):
 		text__edit = self.tabWidget.currentWidget()
-		cursor = text__edit.textCursor()
-		selectedString = cursor.selectedText()
+		selectedString = text__edit.selectedText()
 		return selectedString
 
 #--------------------File Menu Functions------------------#	
 	
 	def newFile_(self):
-		self.textEdit = QtGui.QTextEdit(self.tabWidget)
+		self.textEdit = Qsci.QsciScintilla(self.tabWidget)
 		self.tabWidget.addTab(self.textEdit, "Untitled " + str(self.tabWidget.count()+1))
+
+	def newWindow_(self):
+		Editor()
 
 	def open_(self):
 		oldFileName = QtGui.QFileDialog.getOpenFileName(self, "Open File")
 		file = open(oldFileName, 'r')
 		
-		self.textEdit = QtGui.QTextEdit(self.tabWidget)
+		self.textEdit = Qsci.QsciScintilla(self.tabWidget)
 		self.tabWidget.addTab(self.textEdit, os.path.basename(oldFileName))
 		
 		self.fileList.append(oldFileName)
-
+		
 		data = file.read();
 		self.textEdit.setText(data)
 
@@ -281,7 +286,7 @@ class Editor(QtGui.QMainWindow):
 			for file_path in self.fileList:
 				if self.tabName == os.path.basename(file_path):
 					saveFile = open(file_path, 'w')
-					data = self.tabWidget.currentWidget().toPlainText()					
+					data = self.tabWidget.currentWidget().text()					
 					saveFile.write(data)
 					saveFile.close()
 
@@ -294,13 +299,14 @@ class Editor(QtGui.QMainWindow):
 		
 		self.fileList.append(newFileName)	
 
-		data = self.tabWidget.currentWidget().toPlainText()
+		data = self.tabWidget.currentWidget().text()
 		file.write(data)
 		
 		file.close()
 	
 	def closeTab_(self):
 		self.tabWidget.removeTab(self.tabWidget.currentIndex())
+
 		if self.tabWidget.count() == 0:
 			sys.exit()
 
@@ -370,19 +376,27 @@ class Editor(QtGui.QMainWindow):
 			for text_edit in text_edit_s:
 				text_edit.setFont(font)
 	
-	def setTabWidth_(self,action_group, action2, action4, action8):
+	def setTabWidth_(self,action_group, action2, action4, action8, action12, action16):
 		if action_group.checkedAction() is action2:
 			text_edit_s = (self.tabWidget.widget(i) for i in range(self.tabWidget.count())) 
 			for text_edit in text_edit_s:
-				text_edit.setTabStopWidth(20)
+				text_edit.setTabWidth(2)
 		elif action_group.checkedAction() is action4:
 			text_edit_s = (self.tabWidget.widget(i) for i in range(self.tabWidget.count())) 
 			for text_edit in text_edit_s:
-				text_edit.setTabStopWidth(40)
+				text_edit.setTabWidth(4)
 		elif action_group.checkedAction() is action8:
 			text_edit_s = (self.tabWidget.widget(i) for i in range(self.tabWidget.count())) 
 			for text_edit in text_edit_s:
-				text_edit.setTabStopWidth(80)
+				text_edit.setTabWidth(8)
+		elif action_group.checkedAction() is action12:
+			text_edit_s = (self.tabWidget.widget(i) for i in range(self.tabWidget.count())) 
+			for text_edit in text_edit_s:
+				text_edit.setTabWidth(12)
+		elif action_group.checkedAction() is action16:
+			text_edit_s = (self.tabWidget.widget(i) for i in range(self.tabWidget.count())) 
+			for text_edit in text_edit_s:
+				text_edit.setTabWidth(16)
 
 #-----------------//Format Menu Functions//---------------#	
 
